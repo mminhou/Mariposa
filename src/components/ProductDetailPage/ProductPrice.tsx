4f958da60a6572d95a6b5a0827198ceaa034ad27
R _ R <exit19093@gmail.com>
2025-01-06 13:36:13 +0900

    Fix: 제품가격 number type을 사용하도록 수정

--- src/components/ProductDetailPage/ProductPrice.tsx
+++ src/components/ProductDetailPage/ProductPrice.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 interface ProductPriceProps {
-  price: string;
+  price: number;
 }
 
 const ProductPrice = ({ price }: ProductPriceProps) => {
   return (
     <p className="text-lg font-medium text-gray-900 text-left mt-4 border-b-2 pb-2 border-gray-600">
-      {price.toLocaleString()} 원
+      {String(price).toLocaleString()} 원
     </p>
   );
 };
